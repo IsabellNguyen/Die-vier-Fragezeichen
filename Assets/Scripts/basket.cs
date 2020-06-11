fde2e5b69dfba14601ed66f82ff1b09cea2759c6
isabell nguyen <isabell.nguyen@stud.h-da.de>
2020-06-11 18:14:39 +0200

    Unity Sound/Collision Test
    + set background
    + added sprite flower and basket
    + added ambient sound
    + added drag/drop function for flower
    + added collision and sound for flower over basket

---
+++ Assets/Scripts/basket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.tag = "basket";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*void OnMouseDown()
+    {
+        Debug.Log("Sprite Clicked");
+        gameObject.GetComponent<AudioSource>().Play();
+    }*/
+
+
+}
