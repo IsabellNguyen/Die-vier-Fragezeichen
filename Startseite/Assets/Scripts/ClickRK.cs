0034ad0b4f50714288e0a4d714f4884e2a1ac9b5
isabell nguyen <isabell.nguyen@stud.h-da.de>
2020-06-29 21:44:10 +0200

    Startseite + Animation
    + Lichtstrahl erscheint, wenn man über Rotkäppchen hovert
    + switcht zu neuer Szene/Seite wenn man auf Rotkäppchen klickt
    + Schmetterling Sprite Animation

--- Startseite/Assets/Scripts/ClickRK.cs
+++ Startseite/Assets/Scripts/ClickRK.cs
@@ -1,36 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClickRK : MonoBehaviour
 {
+    GameObject LichtRK;
+    //LichtRK = GameObject.Find("LichtRK");
     void OnMouseDown()
     {
         Debug.Log("You have clicked Rotkäppchen!");
+        //Scene.ChangeToScene(1);
+        SceneManager.LoadScene(1);
     }
 
-        void OnMouseOver()
+    void OnMouseOver()
     {
         //Debug.Log("Mouse is over Rotkäppchen.");
-        transform.localScale = new Vector3(1.2F, 1.2F, 1.2F);
+        transform.localScale = new Vector3(1.1F, 1.1F, 1.1F);
+        LichtRK.SetActive(true);
     }
 
     void OnMouseExit()
     {
         //Debug.Log("Mouse is no longer on Rotkäppchen.");
         transform.localScale = new Vector3(1F, 1F, 1F);
+        LichtRK.SetActive(false);
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LichtRK = GameObject.Find("LichtRK");
+        LichtRK.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //GameObject.Find("LichtRK");
+        //LichtRK = GameObject.Find("LichtRK");
     }
 }
